5b9d7e67ff14707861855f9c78133db7563456ac
Juha <juha.raisala@gmail.com>
2015-10-27 14:32:05 +0200

    jquerya lisaa

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -1,7 +1,39 @@
+//Wait document.ready event
+$(document).ready(function(){
+    console.log("jquery onload triggered");
+});
+
+$(document).ready(domReady);
+function domReady(){
+}
+
+
+
+
+
+
+/*
+
 console.log("jeejeeJEE");
 window.onload = function(event){
 
   console.log(event);
   para1.innerHTML = "Changed from 35";
   para1.style.backgroundColor = "yellow";
+  
+
 }
+
+window.onload = domReadyjeejee;
+function domReadyjeejee(event) {
+  return ;
+}
+
+function someFunction(nimi){
+    console.log(nimi);
+}
+
+someFunction(21);
+someFunction("Juha");
+*/
+
